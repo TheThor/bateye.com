9b913c84dbcf6b9ef457957b7baf04f5f8520e8b
Pedro Tentugal <pedrotentugal@gmail.com>
2014-01-21 22:16:47 +0000

    Adição de catalogue.pdf

---
+++ apps/frontend/modules/collection/templates/indexSuccess.php
@@ -0,0 +1,35 @@
+<div id="collect_container">
+    <div class="span11 collections-imag">
+        <div id="porto-colect">
+            <a href="porto_collection.php"><img class="responsive-img" src="images/portocollection_branco.png"></img></a>
+        </div>
+        <div id="london-collect">
+            <a href=""><img class="responsive-img" src="images/londoncollection_branco.png"></img></a>
+        </div>
+    </div>
+    <div class="clear"></div>
+</div>
+<script type="text/javascript" src="js/sidebuttons.js"></script>
+<div class="span11" id="footer" style="<?php if ($uri_url=="" || $uri_url=="index.php") echo 'display: none';?>">
+    <div id="bar">
+        <div id="logos">
+            <a href="https://twitter.com/bat_eye"><img src="images/icons/bateye_twitter.png" alt="Twitter Logo"></a>
+            <a href="http://www.facebook.com/BatEye.com.pt"><img src="images/icons/bateye_facebook.png" alt="Facebook Logo"></a>
+            <a href="http://www.youtube.com/user/bateyevideos"><img src="images/icons/bateye_youtube.png" alt="Youtube logo"></a>
+            <a href="http://www.behance.net/Bateye"><img src="images/icons/bateye_behance.png" alt="Behance Logo"></a>
+            <a href="http://instagram.com/bateye"><img src="images/icons/bateye_instagram.png" alt="Instagram Logo"></a>
+            <a href="http://www.linkedin.com/company/bat-eye"><img src="images/icons/bateye_linkedin.png" alt="Linkdin logo"></a>
+        </div>
+    </div>
+    <div class="span11 center-text white-text latobold">
+        <p style="font-size:15px;">+<a href="contact.php" style="font-size:14px;">INFO</a></p>
+          <span class="latolight">info@bateye.com<br />
+          +351 915790818</span>
+    </div>
+    <div id="rights" class="latolight">
+        © 2013 <span class="latoitalic">bat eye, Ltd.</span><br />
+        All rights reserved.
+    </div>
+</div>
+
+</div>
