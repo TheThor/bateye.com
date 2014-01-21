40ae4e1a6ead6039f07b2c658f66655e02452d3f
Pedro Tentugal <pedrotentugal@gmail.com>
2014-01-21 22:16:47 +0000

    Adição de catalogue.pdf

---
+++ apps/frontend/modules/collection/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * collection actions.
+ *
+ * @package    bateye
+ * @subpackage collection
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class collectionActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
