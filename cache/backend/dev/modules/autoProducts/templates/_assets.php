cba34397411203438b48df011d6f181921320853
Pedro <pedrotentugal@gmail.com>
2014-02-09 16:27:16 +0000

    Correções no _list template - Problema no path dos partials

--- cache/backend/dev/modules/autoProducts/templates/_assets.php
+++ cache/backend/dev/modules/autoProducts/templates/_assets.php
@@ -1,3 +1,2 @@
- 
-<?php use_stylesheet('/sfDoctrinePlugin/css/global.css', 'first') ?> 
-<?php use_stylesheet('/sfDoctrinePlugin/css/default.css', 'first') ?> 
+	<?php use_stylesheet('/sfDoctrinePlugin/css/global.css', 'first') ?>
+	<?php use_stylesheet('/sfDoctrinePlugin/css/default.css', 'first') ?>
