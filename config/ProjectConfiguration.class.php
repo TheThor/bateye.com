d3845e3342909d46627ce4ba15d4b463bc69598e
Pedro <pedrotentugal@gmail.com>
2014-05-26 23:15:57 +0100

    Finally up in the air

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,7 +1,7 @@
 <?php
-
-require_once dirname(__FILE__).'/../symfony/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once dirname(__DIR__).'/lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
+
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
@@ -10,6 +10,9 @@ class ProjectConfiguration extends sfProjectConfiguration
   {
     $this->enablePlugins('sfDoctrinePlugin');
     $this->enablePlugins('sfDoctrineGuardPlugin');
+    $this->setWebDir('../public_html');
+	  $this->setCacheDir($this->getRootDir().'/cache'); //CORRECTO
+	  $this->setLogDir($this->getRootDir().'/log');//CORRECTO
   }
 
     static public function registerPHPmailer()
@@ -18,7 +21,10 @@ class ProjectConfiguration extends sfProjectConfiguration
         {
             return;
         }
-
+	    sfConfig::add(array(
+		    'sf_web_dir'      => SF_ROOT_DIR.'/../www',
+		    'sf_upload_dir'   => SF_ROOT_DIR.'/../www/'.sfConfig::get('sf_upload_dir_name'),
+	    ));
         set_include_path(sfConfig::get('sf_lib_dir').'/vendor'.PATH_SEPARATOR.get_include_path());
         require_once sfConfig::get('sf_lib_dir').'/vendor/PHPMailer/class.phpmailer.php';
         self::$phpMailer  = true;
