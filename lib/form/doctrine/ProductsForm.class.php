68e7ccc146f5d053426dba36c0629bcd0d7d86e2
Pedro <pedrotentugal@gmail.com>
2014-06-03 00:56:15 +0100

    Added file upload motherfuckersudo php symfony project:permissionssudo php symfony project:permissions

--- lib/form/doctrine/ProductsForm.class.php
+++ lib/form/doctrine/ProductsForm.class.php
@@ -10,7 +10,31 @@
  */
 class ProductsForm extends BaseProductsForm
 {
-  public function configure()
-  {
-  }
+	public function configure()
+	{
+		unset($this['created_at'], $this['updated_at']);
+		$this->setWidget('main_img_path', new sfWidgetFormInputFileEditable(array(
+			'label' => 'Product image',
+			'file_src' => '/images/mosaics/'.$this->getObject()->getCollections().'/'.$this->getObject()->getMainImgPath(),
+			'is_image'  => true,
+			'edit_mode' => !$this->isNew(),
+			'template'  => '<div>%file%<br />%input%<br />%delete% %delete_label%</div>'
+		)));
+		//OR
+		//$this->widgetSchema['main_img_path'] = new sfWidgetFormInputFile(array(
+		// 'label' => 'Product Image'
+		//));
+		$this->setValidator('main_img_path', new sfValidatorFile(array(
+			'required'   => false,
+			'path'       => sfConfig::get('sf_upload_dir') . '/images/mosaics/' . $this->getObject()->getCollections().'/',
+			'mime_types' => 'web_images',
+		)));
+//		OR
+//		$this->validatorSchema['logo'] = new sfValidatorFile(array(
+//			'required'   => false,
+//			'path'       => sfConfig::get('sf_upload_dir').'/jobs',
+//			'mime_types' => 'web_images',
+//		));
+		$this->validatorSchema['main_img_path_delete'] = new sfValidatorPass();
+	}
 }
