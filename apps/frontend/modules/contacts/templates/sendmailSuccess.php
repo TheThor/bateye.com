682ef5d8b78fe852a0083f785101811683235a7a
Pedro <pedrotentugal@gmail.com>
2015-05-05 00:02:43 +0100

    Saving status of sendmailsuccess templat. Works correctly this way

--- apps/frontend/modules/contacts/templates/sendmailSuccess.php
+++ apps/frontend/modules/contacts/templates/sendmailSuccess.php
@@ -4,36 +4,44 @@
  * User: ptentugal
  * Date: 12/16/13
  * Time: 12:27 PM
+ * TODO: Move this to Controller
  */
-if(isset($_POST['contact']) && $_POST['contact']=="true"){
+if (isset($_POST['contact']) && $_POST['contact'] == "true") {
     $email = "marcosousa@bateye.com";
     $name = "Marco Sousa";
     $email_from = $_POST['email'];
     $name_from = $_POST['nome'];
     $assunto = $_POST['assunto'];
     $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-    $message = "Nome: " .$_POST['nome'] . "\n Email: " . $email_from . "\n Cidade: " . $_POST['cidade'] . "\n Mensagem: " . $_POST['mensagem'] . "\nTelefone: ". $_POST['telefone'];
+    $message = "Nome: " . $_POST['nome'] . "\n Email: " . $email_from . "\n Cidade: " . $_POST['cidade'] . "\n Mensagem: " . $_POST['mensagem'] . "\nTelefone: " . $_POST['telefone'];
 
     $mail = new PHPMailer(true);
     $mail->IsSMTP(); // telling the class to use SMTP
-    $mail->SMTPAuth = true; // enable SMTP authentication
-    $mail->Host = "smtp.bateye.com"; // sets GMAIL as the SMTP server
-    $mail->Port = 25; // set the SMTP port for the GMAIL server
-    $mail->Username = "bateye"; // GMAIL username
-    $mail->Password = "att314"; // GMAIL password
+
+//    $mail->SMTPDebug  = 2;
+
+    $mail->Mailer = "smtp";
+    $mail->SMTPKeepAlive = true;
+
+    $mail->SMTPAuth = true;                  // enable SMTP authentication
+    $mail->SMTPSecure = "tls";                 // sets the prefix to the servier
+    $mail->Host = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+    $mail->Port = 587;                   // set the SMTP port for the GMAIL server
+    $mail->Username = "salesbateye@gmail.com";  // GMAIL username
+    $mail->Password = "M@rc01325";            // GMAIL password
+
     $mail->AddAddress($email, $name);
     $mail->SetFrom($email_from, $name_from);
     $mail->Subject = $assunto;
     $mail->Body = $message;
 
-    try{
+    try {
         $mail->Send();
         echo "true";
-    } catch(Exception $e){
-        echo "false". $mail->ErrorInfo;
+    } catch (Exception $e) {
+        echo "false" . $mail->ErrorInfo;
     }
-}
-else{
+} else {
     $email = "marcosousa@bateye.com";
     $name = "Marco Sousa";
     $email_from = $_POST['email'];
@@ -45,21 +53,29 @@ else{
 
     $mail = new PHPMailer(true);
     $mail->IsSMTP(); // telling the class to use SMTP
-    $mail->SMTPSecure = "tls";
-    $mail->SMTPAuth = true; // enable SMTP authentication
-    $mail->Host = "smtp.live.com"; // sets GMAIL as the SMTP server
-    $mail->Port = 587; // set the SMTP port for the GMAIL server
-    $mail->Username = "bateye.sales@hotmail.com"; // GMAIL username
-    $mail->Password = "Portocollection"; // GMAIL password
+
+//    $mail->SMTPDebug  = 2;
+
+    $mail->Mailer = "smtp";
+    $mail->SMTPKeepAlive = true;
+
+    $mail->SMTPAuth = true;                  // enable SMTP authentication
+    $mail->SMTPSecure = "tls";                 // sets the prefix to the servier
+    $mail->Host = "smtp.gmail.com";      // sets GMAIL as the SMTP server
+    $mail->Port = 587;                   // set the SMTP port for the GMAIL server
+    $mail->Username = "salesbateye@gmail.com";  // GMAIL username
+    $mail->Password = "M@rc01325";            // GMAIL password
+
     $mail->AddAddress($email, $name);
     $mail->SetFrom($email_from, $name_from);
     $mail->Subject = $assunto;
     $mail->Body = $message;
 
-    try{
+
+    try {
         $mail->Send();
         echo "true";
-    } catch(Exception $e){
-        echo "false". $mail->ErrorInfo;
+    } catch (Exception $e) {
+        echo "false" . $mail->ErrorInfo;
     }
 }
