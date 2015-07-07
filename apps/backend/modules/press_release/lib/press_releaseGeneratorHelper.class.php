8d1a9f2dba85fbf6742a69821af05f729b5dbe55
Pedro <pedrotentugal@gmail.com>
2015-07-07 21:42:22 +0100

    Adicionado modulo de backend press_release; Modificado .gitignore e removidas pastas do git que não são versionadas; modificado schema.yml para realizar migração

---
+++ apps/backend/modules/press_release/lib/press_releaseGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * press_release module helper.
+ *
+ * @package    bateye
+ * @subpackage press_release
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class press_releaseGeneratorHelper extends BasePress_releaseGeneratorHelper
+{
+}
