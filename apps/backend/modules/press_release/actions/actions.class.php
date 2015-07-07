02b387151cf55675b6aa7e1b0c88b506a3b48af0
Pedro <pedrotentugal@gmail.com>
2015-07-07 21:42:22 +0100

    Adicionado modulo de backend press_release; Modificado .gitignore e removidas pastas do git que não são versionadas; modificado schema.yml para realizar migração

---
+++ apps/backend/modules/press_release/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/press_releaseGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/press_releaseGeneratorHelper.class.php';
+
+/**
+ * press_release actions.
+ *
+ * @package    bateye
+ * @subpackage press_release
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class press_releaseActions extends autoPress_releaseActions
+{
+}
---
+++ apps/backend/modules/press_release/lib/press_releaseGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * press_release module configuration.
+ *
+ * @package    bateye
+ * @subpackage press_release
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class press_releaseGeneratorConfiguration extends BasePress_releaseGeneratorConfiguration
+{
+}
