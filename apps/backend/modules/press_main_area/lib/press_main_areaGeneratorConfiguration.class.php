e2e9c69b021367574982ec123168db3953d8d8dd
Pedro <pedrotentugal@gmail.com>
2015-08-04 23:02:03 +0100

    Add images

---
+++ apps/backend/modules/press_main_area/lib/press_main_areaGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * press_main_area module configuration.
+ *
+ * @package    bateye
+ * @subpackage press_main_area
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class press_main_areaGeneratorConfiguration extends BasePress_main_areaGeneratorConfiguration
+{
+}
