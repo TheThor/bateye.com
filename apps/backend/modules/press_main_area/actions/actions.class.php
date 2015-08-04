1f1ebc8ba4d82635b6cbcda7b969dec5d44fef4f
Pedro <pedrotentugal@gmail.com>
2015-08-04 23:02:03 +0100

    Add images

---
+++ apps/backend/modules/press_main_area/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/press_main_areaGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/press_main_areaGeneratorHelper.class.php';
+
+/**
+ * press_main_area actions.
+ *
+ * @package    bateye
+ * @subpackage press_main_area
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class press_main_areaActions extends autoPress_main_areaActions
+{
+}
