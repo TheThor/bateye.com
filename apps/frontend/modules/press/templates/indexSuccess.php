d9f65bc9d4e7ac0de7991c1347e8f982a9c6a319
Pedro <pedrotentugal@gmail.com>
2015-08-07 19:48:17 +0100

    Press login area properly styled

--- apps/frontend/modules/press/templates/indexSuccess.php
+++ apps/frontend/modules/press/templates/indexSuccess.php
@@ -4,28 +4,34 @@
 <?php include(sfConfig::get('sf_app_template_dir').'/menuSecondary.php') ?>
 <?php slot(
     'title',
-    sprintf('Bat Eye - Press Area'))
+    sprintf('Bat Eye - Press Magazines'))
 ?>
     <style>
         .form-control {
-            height: 20px;
+            height: 25px;
+        }
+        label {
+            height: 25px;
+            margin-bottom: 0px;
         }
         input[type=text], input[type=password] {
             width 50%;
         }
+        .margin-top-normal {
+            margin-top: 30px;
+        }
     </style>
     <div class="container">
-        <div class="col-md-9 col-centered">
+        <div class="col-md-8 col-centered margin-top-normal">
             <div class="col-md-6">
                 <?php echo get_partial('sfGuardAuth/signin_form', array('form' => $form)) ?>
             </div>
             <div class="col-md-4 col-md-push-2">
-                <p>Any other content needed please contact.</p>
-                <p>RAQUEL DIAS - Marketing Director & Client Relations</p>
-
-                <p> raqueldias@bateye.com | +351 227312657 <br/>
-                    info@bateye.com | www.bateye.com <br/>
-                </p>
+                <p>Any other content needed please contact:</p>
+                <p>RAQUEL DIAS - Marketing Director <br>
+                    raqueldias@bateye.com | +351 227312657 <br>
+                    info@bateye.com <br>
+                </>
             </div>
             <div class="clearfix"></div>
         </div>
